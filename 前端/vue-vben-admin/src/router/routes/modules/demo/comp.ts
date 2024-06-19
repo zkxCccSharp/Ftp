2bc0922fb738df33c860519cf1444f1aff170c8b
zkx <5032234@qq.com>
2024-06-19 22:46:03 +0800

    补充 前端

---
+++ 前端/vue-vben-admin/src/router/routes/modules/demo/comp.ts
@@ -0,0 +1,210 @@
+import type { AppRouteModule } from '@/router/types';
+
+import { getParentLayout, LAYOUT } from '@/router/constant';
+import { t } from '@/hooks/web/useI18n';
+
+const comp: AppRouteModule = {
+  path: '/comp',
+  name: 'Comp',
+  component: LAYOUT,
+  redirect: '/comp/basic',
+  meta: {
+    orderNo: 30,
+    icon: 'ion:layers-outline',
+    title: t('routes.demo.comp.comp'),
+  },
+
+  children: [
+    {
+      path: 'allTask',
+      name: 'allTask',
+      component: () => import('@/views/demo/table/Basic.vue'),
+      meta: {
+        title: t('routes.demo.table.allTask'),
+      },
+    },
+    {
+      path: 'uploadTask',
+      name: 'uploadTask',
+      component: () => import('@/views/demo/form/UseForm.vue'),
+      meta: {
+        title: t('routes.demo.table.uploadTask'),
+      },
+    },
+    {
+      path: 'downloadTask',
+      name: 'downloadTask',
+      component: () => import('@/views/demo/table/EditRowTable.vue'),
+      meta: {
+        title: t('routes.demo.table.downloadTask'),
+      },
+    },
+    {
+      path: 'table',
+      name: 'TableDemo',
+      redirect: '/comp/table/basic',
+      component: getParentLayout('TableDemo'),
+      meta: {
+        // icon: 'carbon:table-split',
+        title: t('routes.demo.table.table'),
+      },
+
+      children: [
+        {
+          path: 'basic',
+          name: 'ListBasicPage',
+          component: () => import('@/views/demo/page/list/basic/index.vue'),
+          meta: {
+            title: t('routes.demo.page.listBasic'),
+          },
+        },
+        {
+          path: 'basic',
+          name: 'TableBasicDemo',
+          component: () => import('@/views/demo/table/Basic.vue'),
+          meta: {
+            title: t('routes.demo.table.basic'),
+          },
+        },
+        {
+          path: 'treeTable',
+          name: 'TreeTableDemo',
+          component: () => import('@/views/demo/table/TreeTable.vue'),
+          meta: {
+            title: t('routes.demo.table.treeTable'),
+          },
+        },
+        {
+          path: 'fetchTable',
+          name: 'FetchTableDemo',
+          component: () => import('@/views/demo/table/FetchTable.vue'),
+          meta: {
+            title: t('routes.demo.table.fetchTable'),
+          },
+        },
+        {
+          path: 'fixedColumn',
+          name: 'FixedColumnDemo',
+          component: () => import('@/views/demo/table/FixedColumn.vue'),
+          meta: {
+            title: t('routes.demo.table.fixedColumn'),
+          },
+        },
+        {
+          path: 'customerCell',
+          name: 'CustomerCellDemo',
+          component: () => import('@/views/demo/table/CustomerCell.vue'),
+          meta: {
+            title: t('routes.demo.table.customerCell'),
+          },
+        },
+        {
+          path: 'formTable',
+          name: 'FormTableDemo',
+          component: () => import('@/views/demo/table/FormTable.vue'),
+          meta: {
+            title: t('routes.demo.table.formTable'),
+          },
+        },
+        {
+          path: 'useTable',
+          name: 'UseTableDemo',
+          component: () => import('@/views/demo/table/UseTable.vue'),
+          meta: {
+            title: t('routes.demo.table.useTable'),
+          },
+        },
+        {
+          path: 'refTable',
+          name: 'RefTableDemo',
+          component: () => import('@/views/demo/table/RefTable.vue'),
+          meta: {
+            title: t('routes.demo.table.refTable'),
+          },
+        },
+        {
+          path: 'multipleHeader',
+          name: 'MultipleHeaderDemo',
+          component: () => import('@/views/demo/table/MultipleHeader.vue'),
+          meta: {
+            title: t('routes.demo.table.multipleHeader'),
+          },
+        },
+        {
+          path: 'mergeHeader',
+          name: 'MergeHeaderDemo',
+          component: () => import('@/views/demo/table/MergeHeader.vue'),
+          meta: {
+            title: t('routes.demo.table.mergeHeader'),
+          },
+        },
+        {
+          path: 'expandTable',
+          name: 'ExpandTableDemo',
+          component: () => import('@/views/demo/table/ExpandTable.vue'),
+          meta: {
+            title: t('routes.demo.table.expandTable'),
+          },
+        },
+        {
+          path: 'fixedHeight',
+          name: 'FixedHeightDemo',
+          component: () => import('@/views/demo/table/FixedHeight.vue'),
+          meta: {
+            title: t('routes.demo.table.fixedHeight'),
+          },
+        },
+        {
+          path: 'footerTable',
+          name: 'FooterTableDemo',
+          component: () => import('@/views/demo/table/FooterTable.vue'),
+          meta: {
+            title: t('routes.demo.table.footerTable'),
+          },
+        },
+        {
+          path: 'editCellTable',
+          name: 'EditCellTableDemo',
+          component: () => import('@/views/demo/table/EditCellTable.vue'),
+          meta: {
+            title: t('routes.demo.table.editCellTable'),
+          },
+        },
+        {
+          path: 'editRowTable',
+          name: 'EditRowTableDemo',
+          component: () => import('@/views/demo/table/EditRowTable.vue'),
+          meta: {
+            title: t('routes.demo.table.editRowTable'),
+          },
+        },
+        {
+          path: 'authColumn',
+          name: 'AuthColumnDemo',
+          component: () => import('@/views/demo/table/AuthColumn.vue'),
+          meta: {
+            title: t('routes.demo.table.authColumn'),
+          },
+        },
+        {
+          path: 'resizeParentHeightTable',
+          name: 'ResizeParentHeightTable',
+          component: () => import('@/views/demo/table/ResizeParentHeightTable.vue'),
+          meta: {
+            title: t('routes.demo.table.resizeParentHeightTable'),
+          },
+        },
+        {
+          path: 'vxeTable',
+          name: 'VxeTableDemo',
+          component: () => import('@/views/demo/table/VxeTable.vue'),
+          meta: {
+            title: t('routes.demo.table.vxeTable'),
+          },
+        },
+      ],
+    },
+  ],
+};
+
+export default comp;
---
+++ 前端/vue-vben-admin/src/views/demo/table/tableData.tsx
@@ -0,0 +1,549 @@
+import { optionsListApi } from '@/api/demo/select';
+import { FormProps, FormSchema, BasicColumn } from '@/components/Table';
+import { VxeFormItemProps, VxeGridPropTypes } from '@/components/VxeTable';
+import { ref } from 'vue';
+import { Input } from 'ant-design-vue';
+
+export function getBasicColumns(): BasicColumn[] {
+  return [
+    {
+      title: 'ID',
+      dataIndex: 'Id',
+      fixed: 'left',
+      width: 150,
+    },
+    {
+      title: '状态',
+      dataIndex: 'State',
+      width: 100,
+      filters: [
+        { text: 'Start', value: 'Start' },
+        { text: 'Done', value: 'Done' },
+        { text: 'Exception', value: 'Exception' },
+      ],
+    },
+    {
+      title: '操作类型',
+      width: 140,
+      dataIndex: 'OperType',
+      filters: [
+        { text: 'DownloadDirectory', value: 'DownloadDirectory' },
+        { text: 'DownloadFile', value: 'DownloadFile' },
+        { text: 'UploadDirectory', value: 'UploadDirectory' },
+        { text: 'UploadFile', value: 'UploadFile' },
+      ],
+    },
+    {
+      title: '创建时间',
+      width: 150,
+      sorter: true,
+      dataIndex: 'CreateTime',
+    },
+    {
+      title: '开始时间',
+      dataIndex: 'StartTime',
+      width: 150,
+      sorter: true,
+    },
+    {
+      title: '结束时间',
+      width: 150,
+      sorter: true,
+      dataIndex: 'DoneTime',
+    },
+    {
+      title: '服务器配置',
+      width: 150,
+      dataIndex: 'RemoteIpPort',
+    },
+    {
+      title: '操作参数',
+      dataIndex: 'OperParam',
+    },
+    {
+      title: '错误信息',
+      width: 150,
+      dataIndex: 'ErrorMsg',
+      defaultHidden: true,
+    },
+    {
+      title: '原始任务',
+      width: 150,
+      sorter: true,
+      dataIndex: 'AncestorTaskId',
+      defaultHidden: true,
+    },
+    {
+      title: '会话Id',
+      width: 150,
+      sorter: true,
+      dataIndex: 'SessionId',
+      defaultHidden: true,
+    },
+    {
+      title: '会话Id',
+      width: 150,
+      sorter: true,
+      dataIndex: 'SessionId',
+      defaultHidden: true,
+    },
+  ];
+}
+
+export function getBasicShortColumns(): BasicColumn[] {
+  return [
+    {
+      title: 'ID',
+      width: 150,
+      dataIndex: 'id',
+      sorter: true,
+      sortOrder: 'ascend',
+    },
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      width: 120,
+    },
+    {
+      title: '地址',
+      dataIndex: 'address',
+    },
+    {
+      title: '编号',
+      dataIndex: 'no',
+      width: 80,
+    },
+  ];
+}
+
+export function getMultipleHeaderColumns(): BasicColumn[] {
+  const testRef = ref('姓名:');
+  return [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      width: 200,
+    },
+    {
+      title: '姓名',
+      customHeaderRender() {
+        return (
+          <Input placeholder="输入值 更新 自定义title" size="small" v-model:value={testRef.value} />
+        );
+      },
+      dataIndex: 'name',
+      width: 120,
+    },
+    {
+      title: '地址',
+      dataIndex: 'address',
+      sorter: true,
+      children: [
+        {
+          title: '编号',
+          customHeaderRender(column) {
+            // 【自定义渲染的】
+            return (
+              <div>
+                _ <span style="background: #f00; color: #fff;">{testRef.value}</span> _
+                {column.customTitle}
+              </div>
+            );
+          },
+          dataIndex: 'no',
+          width: 120,
+          filters: [
+            { text: 'Male', value: 'male', children: [] },
+            { text: 'Female', value: 'female', children: [] },
+          ],
+        },
+
+        {
+          title: '开始时间',
+          dataIndex: 'beginTime',
+          width: 120,
+        },
+        {
+          title: '结束时间',
+          dataIndex: 'endTime',
+          width: 120,
+        },
+      ],
+    },
+  ];
+}
+
+export function getCustomHeaderColumns(): BasicColumn[] {
+  return [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      helpMessage: 'headerHelpMessage方式1',
+      width: 200,
+    },
+    {
+      // title: '姓名',
+      dataIndex: 'name',
+      width: 120,
+    },
+    {
+      // title: '地址',
+      dataIndex: 'address',
+      width: 120,
+      sorter: true,
+    },
+
+    {
+      title: '编号',
+      dataIndex: 'no',
+      width: 120,
+      filters: [
+        { text: 'Male', value: 'male', children: [] },
+        { text: 'Female', value: 'female', children: [] },
+      ],
+    },
+    {
+      title: '开始时间',
+      dataIndex: 'beginTime',
+      width: 120,
+    },
+    {
+      title: '结束时间',
+      dataIndex: 'endTime',
+      width: 120,
+    },
+  ];
+}
+
+export function getEditTaskColumns(): BasicColumn[] {
+  return [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      helpMessage: 'headerHelpMessage方式1',
+      width: 200,
+    },
+    {
+      // title: '姓名',
+      dataIndex: 'name',
+      width: 120,
+    },
+    {
+      // title: '地址',
+      dataIndex: 'address',
+      width: 120,
+      sorter: true,
+    },
+
+    {
+      title: '编号',
+      dataIndex: 'no',
+      width: 120,
+      filters: [
+        { text: 'Male', value: 'male', children: [] },
+        { text: 'Female', value: 'female', children: [] },
+      ],
+    },
+    {
+      title: '开始时间',
+      dataIndex: 'beginTime',
+      width: 120,
+    },
+    {
+      title: '结束时间',
+      dataIndex: 'endTime',
+      width: 120,
+    },
+  ];
+}
+
+const cellContent = (_, index) => ({
+  colSpan: index === 9 ? 0 : 1,
+});
+
+export function getMergeHeaderColumns(): BasicColumn[] {
+  return [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      width: 300,
+      customCell: (_, index) => ({
+        colSpan: index === 9 ? 6 : 1,
+      }),
+    },
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      width: 300,
+      customCell: cellContent,
+    },
+    {
+      title: '地址',
+      dataIndex: 'address',
+      colSpan: 2,
+      width: 120,
+      sorter: true,
+      customCell: (_, index) => ({
+        rowSpan: index === 2 ? 2 : 1,
+        colSpan: index === 3 || index === 9 ? 0 : 1,
+      }),
+    },
+    {
+      title: '编号',
+      dataIndex: 'no',
+      colSpan: 0,
+      filters: [
+        { text: 'Male', value: 'male', children: [] },
+        { text: 'Female', value: 'female', children: [] },
+      ],
+      customCell: cellContent,
+    },
+    {
+      title: '开始时间',
+      dataIndex: 'beginTime',
+      width: 200,
+      customCell: cellContent,
+    },
+    {
+      title: '结束时间',
+      dataIndex: 'endTime',
+      width: 200,
+      customCell: cellContent,
+    },
+  ];
+}
+export const getAdvanceSchema = (itemNumber = 6): FormSchema[] => {
+  const arr: FormSchema[] = [];
+  for (let index = 0; index < itemNumber; index++) {
+    arr.push({
+      field: `field${index}`,
+      label: `字段${index}`,
+      component: 'Input',
+      colProps: {
+        xl: 12,
+        xxl: 8,
+      },
+    });
+  }
+  return arr;
+};
+export function getFormConfig(): Partial<FormProps> {
+  return {
+    labelWidth: 100,
+    schemas: [
+      ...getAdvanceSchema(5),
+      {
+        field: `field11`,
+        label: `Slot示例`,
+        slot: 'custom',
+        colProps: {
+          xl: 12,
+          xxl: 8,
+        },
+      },
+    ],
+  };
+}
+export function getBasicData() {
+  return (() => {
+    const arr: any = [];
+    for (let index = 0; index < 40; index++) {
+      arr.push({
+        id: `${index}`,
+        name: 'John Brown',
+        age: `1${index}`,
+        no: `${index + 10}`,
+        address: 'New York No. 1 Lake ParkNew York No. 1 Lake Park',
+        beginTime: new Date().toLocaleString(),
+        endTime: new Date().toLocaleString(),
+      });
+    }
+    return arr;
+  })();
+}
+
+export function getTreeTableData() {
+  return (() => {
+    const arr: any = [];
+    for (let index = 0; index < 40; index++) {
+      arr.push({
+        id: `${index}`,
+        name: 'John Brown',
+        age: `1${index}`,
+        no: `${index + 10}`,
+        address: 'New York No. 1 Lake ParkNew York No. 1 Lake Park',
+        beginTime: new Date().toLocaleString(),
+        endTime: new Date().toLocaleString(),
+        children: [
+          {
+            id: `l2-${index}-1`,
+            name: 'John Brown',
+            age: `1`,
+            no: `${index + 10}`,
+            address: 'New York No. 1 Lake ParkNew York No. 1 Lake Park',
+            beginTime: new Date().toLocaleString(),
+            endTime: new Date().toLocaleString(),
+            children: [
+              {
+                id: `l3-${index}-1-1`,
+                name: 'John Brown',
+                age: `11`,
+                no: `11`,
+                address: 'New York No. 1 Lake ParkNew York No. 1 Lake Park',
+                beginTime: new Date().toLocaleString(),
+                endTime: new Date().toLocaleString(),
+              },
+              {
+                id: `l3-${index}-1-2`,
+                name: 'John Brown',
+                age: `12`,
+                no: `12`,
+                address: 'New York No. 1 Lake ParkNew York No. 1 Lake Park',
+                beginTime: new Date().toLocaleString(),
+                endTime: new Date().toLocaleString(),
+              },
+            ],
+          },
+          {
+            id: `l2-${index}-2`,
+            name: 'John Brown',
+            age: `2`,
+            no: `${index + 10}`,
+            address: 'New York No. 1 Lake ParkNew York No. 1 Lake Park',
+            beginTime: new Date().toLocaleString(),
+            endTime: new Date().toLocaleString(),
+            children: [
+              {
+                id: `l3-${index}-2-1`,
+                name: 'John Brown',
+                age: `21`,
+                no: `21`,
+                address: 'New York No. 1 Lake ParkNew York No. 1 Lake Park',
+                beginTime: new Date().toLocaleString(),
+                endTime: new Date().toLocaleString(),
+              },
+              {
+                id: `l3-${index}-2-2`,
+                name: 'John Brown',
+                age: `22`,
+                no: `22`,
+                address: 'New York No. 1 Lake ParkNew York No. 1 Lake Park',
+                beginTime: new Date().toLocaleString(),
+                endTime: new Date().toLocaleString(),
+              },
+            ],
+          },
+        ],
+      });
+    }
+    return arr;
+  })();
+}
+
+export const vxeTableColumns: VxeGridPropTypes.Columns = [
+  {
+    title: '序号',
+    type: 'seq',
+    fixed: 'left',
+    width: '50',
+    align: 'center',
+  },
+  {
+    title: '固定列',
+    field: 'name',
+    width: 150,
+    showOverflow: 'tooltip',
+    fixed: 'left',
+  },
+  {
+    title: '自适应列',
+    field: 'address',
+  },
+  {
+    title: '自定义列(自定义导出)',
+    field: 'no',
+    width: 200,
+    showOverflow: 'tooltip',
+    align: 'center',
+    slots: {
+      default: ({ row }) => {
+        const text = `自定义${row.no}`;
+        return [<div class="text-red-500">{text}</div>];
+      },
+    },
+    exportMethod: ({ row }) => {
+      return `自定义${row.no}导出`;
+    },
+  },
+  {
+    title: '自定义编辑',
+    width: 150,
+    field: 'name1',
+    align: 'center',
+    editRender: {
+      name: 'AInput',
+      placeholder: '请点击输入',
+    },
+  },
+  {
+    title: '开始时间',
+    width: 150,
+    field: 'beginTime',
+    showOverflow: 'tooltip',
+    align: 'center',
+  },
+  {
+    title: '结束时间',
+    width: 150,
+    field: 'endTime',
+    showOverflow: 'tooltip',
+    align: 'center',
+  },
+  {
+    width: 160,
+    title: '操作',
+    align: 'center',
+    slots: { default: 'action' },
+    fixed: 'right',
+  },
+];
+
+export const vxeTableFormSchema: VxeFormItemProps[] = [
+  {
+    field: 'field0',
+    title: 'field0',
+    itemRender: {
+      name: 'AInput',
+    },
+    span: 6,
+  },
+  {
+    field: 'field1',
+    title: 'field1',
+    itemRender: {
+      name: 'AApiSelect',
+      props: {
+        api: optionsListApi,
+        resultField: 'list',
+        labelField: 'name',
+        valueField: 'id',
+      },
+    },
+    span: 6,
+  },
+  {
+    span: 12,
+    align: 'right',
+    className: '!pr-0',
+    itemRender: {
+      name: 'AButtonGroup',
+      children: [
+        {
+          props: { type: 'primary', content: '查询', htmlType: 'submit' },
+          attrs: { class: 'mr-2' },
+        },
+        { props: { type: 'default', htmlType: 'reset', content: '重置' } },
+      ],
+    },
+  },
+];
